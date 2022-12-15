3399e5d2c0d9af8bb456ad488385378eddd24f31
Eve Stroud <evestroud89@gmail.com>
2022-12-15 11:00:44 -0500

    Finish Wave 1

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
+  const [complete, setComplete] = useState(false);
+
+  const toggleComplete = () => {
+    setComplete(!complete);
+  };
+
+  const toggleStyle = complete
+    ? 'tasks__item__toggle--completed'
+    : 'tasks__item__toggle';
+
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
+      <button className={toggleStyle} onClick={toggleComplete}>
+        {title}
+      </button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
